34769c8a56752e4143df37c18c3f4ec6389bcaf2
Hadi Chloun <hadi_960@hotmail.com>
2021-02-15 01:28:51 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,16 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  final_results=data.each_with_object({}) do|(outer_key, value), final_array|
+    value.each do |inner_key, names|
+      names.each do |name|
+        if !final_array[name]
+          final_array[name]={}
+        end
+        if !final_array[name][outer_key]
+          !final_array[name][outer_key]=[]
+        end
+        final_array[name][outer_key].push(inner_key.to_s)
+      end
+    end
+  end
 end
